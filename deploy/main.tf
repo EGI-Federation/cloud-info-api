8b7ffe75c737e1598dfcdcc6e439c38f012c6a39
Enol Fernandez <enol.fernandez@egi.eu>
2025-03-11 11:51:44 +0000

    Bringing the changes up to speed with dashboard

--- deploy/main.tf
+++ deploy/main.tf
@@ -1,16 +1,16 @@
-resource "openstack_compute_instance_v2" "cloud-api" {
+resource "openstack_compute_instance_v2" "cloud-info-api" {
   name      = "cloud-info-api"
-  image_id  = var.image_id
+  image_id  = data.openstack_images_image_v2.egi-docker.id
   flavor_id = var.flavor_id
   user_data = file("cloud-init.yaml")
   network {
     uuid = var.net_id
   }
-  security_groups = [openstack_compute_secgroup_v2.secgroup.name, "default"]
+  security_groups = ["HTTP", "motley-cue"]
 }
 
 resource "openstack_compute_secgroup_v2" "secgroup" {
-  name        = "HTTP-cloud-api"
+  name        = "HTTP-cloud-info-api"
   description = "HTTP and HTTPS access"
 
   rule {
@@ -28,13 +28,33 @@ resource "openstack_compute_secgroup_v2" "secgroup" {
   }
 }
 
+resource "openstack_compute_secgroup_v2" "motley-cloud-info-api" {
+  name        = "motley-cue"
+  description = "Open access via ssh-oidc"
+
+  rule {
+    from_port   = 22
+    to_port     = 22
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+  rule {
+    from_port   = 8181
+    to_port     = 8181
+    ip_protocol = "tcp"
+    cidr        = "0.0.0.0/0"
+  }
+
+}
+
 resource "openstack_networking_floatingip_v2" "fip" {
   pool = var.ip_pool
 }
 
 resource "openstack_compute_floatingip_associate_v2" "fip" {
   floating_ip = openstack_networking_floatingip_v2.fip.address
-  instance_id = openstack_compute_instance_v2.cloud-api.id
+  instance_id = openstack_compute_instance_v2.cloud-info-api.id
 }
 
 output "public_ip" {
