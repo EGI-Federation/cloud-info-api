45d36eff19502f46808498f68671400848d97467
Enol Fernández <enol.fernandez@egi.eu>
2025-03-26 15:23:35 +0000

    Move to private object storage (#20)
    * Move to File based storage
    
    * Reload all files every time
    
    * Small script for getting the cloud-info files
    
    * Add file with secrets
    
    * Add the cron and mount the directory with the info
    
    * Add secret
    
    * Add secret and install openstack client
    
    * Linting fixes
    
    * Clean up

---
+++ deploy/fetch-info.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# this will make the update not to happen if any of the intermediate
+# steps fail
+set -e
+
+CLOUD_INFO_DIR="$1"
+CLOUD_INFO_CLOUD="cloud-info"
+CLOUD_INFO_CONTAINER="cloud-info"
+
+DIR="$(mktemp -d)"
+for f in $(openstack --os-cloud "$CLOUD_INFO_CLOUD" \
+	object list "$CLOUD_INFO_CONTAINER" -f json | jq -r -n 'inputs[] | values[]'); do
+  echo "Downloading: $f"
+  openstack --os-cloud "$CLOUD_INFO_CLOUD" object save \
+	  "$CLOUD_INFO_CONTAINER" --file "$DIR/$(dirname "$f").json" "$f"
+done
+
+rsync -a --delete-after "$DIR/" "$CLOUD_INFO_DIR"
+
+rm -rf "$DIR"
